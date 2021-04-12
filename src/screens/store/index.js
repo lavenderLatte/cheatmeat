e2c4cfc551025aeea30c5ececb1f8a3310031a8e
lavenderLatte <tohanara@gmail.com>
2021-04-11 18:06:56 -0700

    added markers, searchbars, and user location to map

--- src/screens/store/index.js
+++ src/screens/store/index.js
@@ -2,17 +2,56 @@ import React from 'react';
 import {
   Text,
   View,
-  StyleSheet 
+  StyleSheet,
+  StatusBar
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import MapView from 'react-native-maps'
-
+import MapView, { Marker } from 'react-native-maps';
+import { SearchBar } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 class Store extends React.Component {
   render() {
     return (
       <>
-        <MapView style={{ ...StyleSheet.absoluteFillObject }}> 
+        <SafeAreaView>
+          <StatusBar backgroundColor="rgba(1.0, 0, 0, 0.2)" translucent />
+          <SearchBar
+          ref='searchBar' 
+          placeholder='Find cheatmeat'
+          barStyle="default"
+          showsCancelButtonWhileEditing={false}
+          // position = absolute
+        />
+        </SafeAreaView>
+
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 34.020755529483175,
+            longitude: -118.28933010210339,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+        >
+
+          <Marker
+            coordinate={{ latitude: 34.04857723789541, longitude: -118.24967692396997 }}
+            title='far ass whole foods'
+            description='This is where to get beyond meat!'
+          ></Marker >
+          <Marker
+            coordinate={{ latitude: 34.0292215152339, longitude: -118.2797176929372 }}
+            title="nearby trader joe's"
+            description='good vegan choices here!'
+          ></Marker > 
+          <Marker
+            coordinate={{ latitude: 34.02288873697428, longitude: -118.29083217294722 }}
+            title="taco bell"
+            description='some vegan choices here!'
+          ></Marker >          
         </MapView>
       </>
     );
